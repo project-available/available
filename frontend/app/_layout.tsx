70c4674002fb1d0798c11ea055333f95e0a7f2da
bptn1919~ <buinhi4321@gmail.com>
2025-10-02 19:35:28 +0700

    feat: implement tab navigation with custom tab bar and screens for Home, Room, Notification, and Profile

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,5 +2,10 @@ import { Stack } from "expo-router";
 import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="history" />
+    </Stack>
+  );
 }
