2d1d1f728724248434bc93ee5e292fa2b01b8492
Yen Nhi <lenguyenyennhi2004@gmail.com>
2025-10-06 15:34:54 +0700

    add unit test for create account

---
+++ backend/db/sqlc/account_test.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	utils "github.com/project-available/available.git/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func createAccount(t *testing.T) Account{
+	arg := create_accountParams{
+		Name: utils.RandomString(15),
+		Role: utils.RandomRole(),
+		Email: utils.RandomEmail(),
+		Password: "12345",
+		Phone: utils.RandomPhone(),
+		StudentID: utils.RandomStudentID(),
+	}
+	account, err := testQuery.create_account(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, arg.Name, account.Name)
+	require.Equal(t, arg.Role, account.Role)
+	require.Equal(t, arg.Email, account.Email)
+	require.Equal(t, arg.Phone, account.Phone)
+	require.Equal(t, arg.StudentID, account.StudentID)
+	require.NotZero(t,account.ID)
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 :=createAccount(t)
+	account2, err := testQuery.get_account(context.Background(), account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.Name, account2.Name)
+	require.Equal(t, account1.Role, account2.Role)
+	require.Equal(t, account1.Email, account2.Email)
+	require.Equal(t, account1.Phone, account2.Phone)
+	require.Equal(t, account1.StudentID, account2.StudentID)
+}
