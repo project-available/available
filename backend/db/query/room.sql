ab17c31395450e4f9df094922e0f29dd40e21e78
Yen Nhi <lenguyenyennhi2004@gmail.com>
2025-10-12 22:24:37 +0700

    API CRUD for Rooms

---
+++ backend/db/query/room.sql
@@ -0,0 +1,24 @@
+-- name: CreateRoom :one
+INSERT INTO rooms (name, location, image)
+VALUES ($1,$2,$3)
+RETURNING *;
+
+-- name: ListRooms :many
+SELECT * FROM rooms
+WHERE is_delete != true
+ORDER BY id
+LIMIT $1 OFFSET $2;
+
+-- name: UpdateRoom :one
+UPDATE rooms
+SET name = $2, location = $3, image = $4
+WHERE id = $1
+AND is_delete != true
+RETURNING *;
+
+-- name: DeleteRoom :one
+UPDATE rooms
+SET is_delete = true
+WHERE id = $1
+AND is_delete != true
+RETURNING *;
